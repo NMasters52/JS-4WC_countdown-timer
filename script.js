a8a8393770be49e882bafa9defbf7796b477030e
Nicholas Masters <168689746+NMasters52@users.noreply.github.com>
2025-03-17 18:11:00 +0000

    feat added a pause button

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 const elements = {
     display: document.getElementById("countDisplay"),
-    countDownBtn: document.getElementById("countDownBtn")
+    countDownBtn: document.getElementById("countDownBtn"),
+    pause: document.getElementById("btn-pause")
 };
 
 let count;
+let intervalID;
 
 function countDown() {
     count = 10;
     displayCount(count);
-  const intervalID =  setInterval(decrimentCount, 1000);
 
-function decrimentCount() {
-    count--;
-    displayCount(count);
-    if (count <= 0) {
+    elements.countDownBtn.innerText = "running";
+
+    intervalID =  setInterval(decrimentCount, 1000);
+
+    function decrimentCount() {
+        count--;
+        displayCount(count);
+        if (count <= 0) {
+            clearInterval(intervalID);
+            elements.countDownBtn.innerText = "reset";
+        }
+    };
+
+};
+
+function pauseCount() {
+    if (intervalID) {
         clearInterval(intervalID);
     }
-};
-
-};
+}
 
 function displayCount(count) {
     elements.display.innerText = count;
@@ -26,6 +38,7 @@ function displayCount(count) {
 
 function intializeApp(){
     elements.countDownBtn.addEventListener("click", countDown);
+    elements.pause.addEventListener("click", pauseCount);
 }
 
 intializeApp();
