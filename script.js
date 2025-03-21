a9523f656a51a439e9098d5baaf4b8354d0b3ebb
Nicholas Masters <168689746+NMasters52@users.noreply.github.com>
2025-03-21 15:49:26 +0000

    add feat. Pause button can pause and reseume timer using state

--- script.js
+++ script.js
@@ -6,14 +6,19 @@ const elements = {
 
 let count;
 let intervalID;
+let isPaused = false;
 
 function countDown() {
+    if (intervalID) {
+        clearInterval(intervalID);
+    }
+
     count = 10;
     displayCount(count);
-
     elements.countDownBtn.innerText = "running";
-
     intervalID =  setInterval(decrimentCount, 1000);
+    isPaused = false;
+};
 
     function decrimentCount() {
         count--;
@@ -24,13 +29,23 @@ function countDown() {
         }
     };
 
-};
+
 
 function pauseCount() {
-    if (intervalID) {
+    isPaused = !isPaused;
+
+    if (intervalID && isPaused) {
         clearInterval(intervalID);
+        intervalID = null;
+        elements.pause.innerText = "resume";
+
+    } else if (!isPaused && count > 0){
+        intervalID = setInterval(decrimentCount, 1000);
+        elements.pause.innerText = "pause";
     }
+
 }
+
 
 function displayCount(count) {
     elements.display.innerText = count;
